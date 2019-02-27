0745a5fd4c9868be6d59e17d3045d087795cc613
prashanth547 <46973195+prashanth547@users.noreply.github.com>
2019-02-26 17:26:55 -0800

    Update tfbackend.tf

--- terraform/dev/tfbackend.tf
+++ terraform/dev/tfbackend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "testbucket-varun"
-    key     = "tf/us-east-1/testbucket-varun-dev.json"
+    bucket  = "prashanthbucket547"
+    key     = "dev.json"
     region  = "us-east-2"
-    profile = "default"
+    profile = "jinka"
   }
 }
